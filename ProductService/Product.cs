62cb896f05f2b86e51e0e6549bea2017da76b90c
Dmitrii Kudriavtcev <dvkudr@gmail.com>
2022-02-07 18:24:10 +0300

    initial commit

---
+++ ProductService/Product.cs
@@ -0,0 +1,6 @@
+namespace ProductService;
+
+public class Product
+{
+    public string Id { get; set;  }
+}
---
+++ ProductService/Program.cs
@@ -0,0 +1,33 @@
+using System.Collections.Concurrent;
+using ProductService;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var products = new ConcurrentDictionary<string, Product>();
+
+app
+    .MapPut("/product/{ID}/add", (string id) =>
+    {
+        var product = new Product{ Id = id };
+        if (products.TryAdd(id, product))
+        {
+            return Results.CreatedAtRoute("get_product", new { id });
+        }
+
+        return Results.Conflict();
+    });
+
+app
+    .MapGet("/product/{ID}", (string id) =>
+    {
+        if (products.TryGetValue(id, out var product))
+        {
+            return Results.Ok(product);
+        }
+
+        return Results.NotFound();
+    })
+    .WithName("get_product");
+
+app.Run();
