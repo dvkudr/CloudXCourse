646b91e28ae0f4101559c5cae865a27329449489
Dmitrii Kudriavtcev <Dmitrii.Kudriavtcev@ihsmarkit.com>
2022-04-15 15:44:51 +0300

    add swagger and scripts

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -5,32 +5,48 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using ProductService;
 
-var builder = WebApplication.CreateBuilder(args);
-
 AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-Sdk.CreateTracerProviderBuilder()
-    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("product-service").AddTelemetrySdk())
-    .AddXRayTraceId()
-    .AddAWSInstrumentation()
-    .AddAspNetCoreInstrumentation()
-    .AddHttpClientInstrumentation()
-    .AddOtlpExporter(options =>
-    {
-        options.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT"));
-    })
-    .Build();
+var products = new ConcurrentDictionary<string, Product>();
 
-Sdk.SetDefaultTextMapPropagator(new AWSXRayPropagator());
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
 
 var app = builder.Build();
 
-var products = new ConcurrentDictionary<string, Product>();
+if (!app.Environment.IsDevelopment())
+{
+    Sdk.CreateTracerProviderBuilder()
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("product-service").AddTelemetrySdk())
+        .AddXRayTraceId()
+        .AddAWSInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT"));
+        })
+        .Build();
+
+    Sdk.SetDefaultTextMapPropagator(new AWSXRayPropagator());
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app
     .MapPut("/product/{ID}/add", (string id) =>
     {
-        /*FindPrimeNumber.Run(100000);*/
         var product = new Product{ Id = id };
         if (products.TryAdd(id, product))
         {
@@ -43,7 +59,6 @@ app
 app
     .MapGet("/product/{ID}", (string id) =>
     {
-        /*FindPrimeNumber.Run(100000);*/
         if (products.TryGetValue(id, out var product))
         {
             return Results.Ok(product);
